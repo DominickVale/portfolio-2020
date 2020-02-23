86be14466b3d3cf054a68200835b1216724d0b7a
Dominick <dominickveil@gmail.com>
2020-02-23 16:13:17 +0100

    Add Highlight style component

--- src/components/shared/styles.js
+++ src/components/shared/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import constants from '../../constants'
 
 export const Button = styled.button`
 padding: 0;
@@ -6,3 +7,6 @@ background: none;
 border: none;
 cursor: pointer;
 `
+
+export const Highlight = styled.span`
+color: ${constants.highlightColor};`
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,9 @@
 export default {
   backgroundColor: '#121111',
+  backgroundObjectsColor: '#F3E7E7',
   navLinkColor: '#D7D7D7',
   navLinkSize: '14px',
   navLinkWeight: '700',
+  descriptionColor: '#A7A7A7',
+  highlightColor: '#D52A24'
 }

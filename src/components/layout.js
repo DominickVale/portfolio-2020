51b98bea05a79cde3e8e93e0ba323d0c03b0f501
Dominick <dominickveil@gmail.com>
2020-03-02 18:41:24 +0100

    Film grain prototype (td: replace with three.js)

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,11 +7,14 @@ import constants from "../constants"
 import 'normalize.css';
 import './styles.css';
 
+import noise from '../../public/images/noise.png'
+import {noiseAnimation} from '../components/shared/animations'
 
 
 const GlobalStyle = createGlobalStyle`
 html{
   scroll-behavior: smooth;
+  background: black;
 }
 
 body{
@@ -19,6 +22,22 @@ body{
   overflow-x: hidden;
   padding: 0 33px;
   color: beige;
+  &::after{
+    content: '';
+    user-select: none;
+    pointer-events: none;
+    display: block;
+    position: fixed;
+    height: 300vh;
+    width: 300vw;
+    z-index: 1;
+    left: -100%;
+    top: -100%;
+    opacity: 0.3;
+    mix-blend-mode: overlay;
+    background-image: url(${noise});
+    animation: ${noiseAnimation} 1s steps(10) alternate infinite;
+  }
   @media only screen and (min-width: 720px){
     padding: 0 62px;
   }

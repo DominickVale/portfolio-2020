8971c90823530492ce65b5b5f03576b11f461397
Dominick <dominickveil@gmail.com>
2020-03-13 09:55:10 +0100

    Add quote

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -26,7 +26,7 @@ const Footer = () => {
           <p>742 Evergreen Terrace, IT</p>
         </Content>
       </ContactInfo>
-      <QuoteSection>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae consequat non diam sed cursus. Eget aliquam egestas faucibus enim at. A nec, malesuada aliquam...</QuoteSection>
+      <QuoteSection>The individual has always had to struggle to keep from being overwhelmed by the tribe. To be your own man is a hard business. If you try it, you'll be lonely often, and sometimes frightened. But no price is too high to pay for the privilege of owning yourself. — Rudyard Kipling</QuoteSection>
       <Icons />
       <DevelopedBy><p>developed & designed by <a href="https://www.facebook.com/dominickveil" target="_blank" rel="noopener noreferrer"><Highlight>Yours truly</Highlight></a>.</p></DevelopedBy>
     </FooterContainer>
